b38c76eb4f7b0b7381ae93611ef88410a584048f
Turnt-SNACO <janderson5@albany.edu>
2017-03-10 16:08:46 -0500

    Added print forward and reverse methods to Linked List as well as addToEnd

--- AndersonJamesH03/src/LinkedList.java
+++ AndersonJamesH03/src/LinkedList.java
@@ -1,4 +1,53 @@
 
 public class LinkedList {
-
+	private Node origin=null;
+	private Node end=null;
+	private int size=0;
+	public LinkedList(){}
+	
+	//returns the size of the list
+	public int getSize(){
+		return size;
+	}
+	
+	//adds node to the end of the list
+	public void addToEnd(Node node) {
+    	if (origin==null){  
+    		origin=node;
+    		end=node;
+    	}else{
+    		Node current = origin;
+    		while(current.getNext()!=null){
+    			current=current.getNext();
+    		}
+    		current.setNext(node);
+    		node.setPrevious(current);
+    		end=node;
+    	}
+    	size++;
+    }
+	
+	//iterates forward through the list and returns a string that is comma delimited containing the content of each node.
+	public String printForward(){
+		String list="";
+		Node current=origin;
+		while(current.getNext()!=null){
+			list+=current.getContent()+", ";
+    		current=current.getNext();
+    	}
+		list+=current.getContent();
+		return list;
+	}
+	
+	//iterates backwards through the list and returns a string that is comma delimited containing the content of each node.
+	public String printReverse(){
+		String list="";
+		Node current=end;
+		while(current.getPrevious()!=null){
+			list+=current.getContent()+", ";
+    		current=current.getPrevious();
+    	}
+		list+=current.getContent();
+		return list;
+	}
 }
