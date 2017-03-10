2b7101196e57950929a12f01bf0e4fb73b8f1e34
Turnt-SNACO <janderson5@albany.edu>
2017-03-10 16:09:11 -0500

    Updated tests.

--- AndersonJamesH03/src/Main.java
+++ AndersonJamesH03/src/Main.java
@@ -1,9 +1,18 @@
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		LinkedList l = new LinkedList();
+		Node ace = new Node(0, "ace");
+		Node club = new Node(1, "club");
+		Node spade = new Node (2, "spade");
+		Node diamond = new Node (3, "diamond");
+		
+		l.addToEnd(ace);
+		l.addToEnd(club);
+		l.addToEnd(spade);
+		l.addToEnd(diamond);
+		
+		System.out.println(l.printForward());
+		System.out.println(l.printReverse());
 	}
-
 }
