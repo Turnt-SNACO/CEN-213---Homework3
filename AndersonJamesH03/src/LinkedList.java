e6103e45fa911e05fd04f547a919b3b032164e80
Turnt-SNACO <janderson5@albany.edu>
2017-03-10 17:00:09 -0500

    Added findNode(ID) and updated documentation

--- AndersonJamesH03/src/LinkedList.java
+++ AndersonJamesH03/src/LinkedList.java
@@ -5,12 +5,21 @@ public class LinkedList {
 	private int size=0;
 	public LinkedList(){}
 	
-	//returns the size of the list
+	/**
+	 * <b><i>getSize</i></b> - Gets number of nodes in the list/
+	 * <p>
+	 * @return <b>size</b> - count of nodes in list.
+	 */
 	public int getSize(){
 		return size;
 	}
 	
-	//adds node to the end of the list
+	/**
+	 * <b><i>addToEnd</i></b> - Adds a node to the end of the list.
+	 * <p>
+	 * @param node - node to be added
+	 * @author james_2pes9af
+	 */
 	public void addToEnd(Node node) {
     	if (origin==null){  
     		origin=node;
@@ -27,8 +36,14 @@ public class LinkedList {
     	size++;
     }
 	
-	//sets the next node of the node behind the current node to be equal 
-	//to the next node of the current effectively deleting from the list
+	/**
+	 * <b><i>deleteNode</i></b> - Deletes a node from the list.
+	 * <p>
+	 * Sets the pointers of the node before and after to point at each other leaving no node pointed to the targeted node for deletion.
+	 * <p>
+	 * @param node - The Node targeted for deletion.
+	 * @author james_2pes9af
+	 */
 	public void deleteNode(Node node){
 		if (node!=origin)
 			node.getPrevious().setNext(node.getNext());
@@ -41,7 +56,15 @@ public class LinkedList {
 		size--;
 	}
 	
-	//searches for node based on content and returns the node
+	/** 
+	 * <b><i>findNode</i></b> - Searches for node based on content
+	 * <p>
+	 * Given a string value this method will find if that node exists in the current list.
+	 * <p>
+	 * @param content - string that is being searched for.
+	 * @return <b>Node</b> with content if exists, else <b>null</b>
+	 * @author james_2pes9af
+	 */
 	public Node findNode(String content){
 		Node current = origin;
 		while(current.getNext()!=null){
@@ -52,9 +75,35 @@ public class LinkedList {
 		}
 		return null;
 	}
-		
-	//iterates forward through the list and returns a string that is comma 
-	//delimited containing the content of each node.
+	
+	/**
+	 * <b><i>findNode</i></b> - Searches for node based on ID
+	 * <p>
+	 * Given an integer value this method will find if that node exists in the current list.
+	 * <p>
+	 * @param ID - integer assigned to node.
+	 * @return <b>Node</b> with ID if exists, else <b>null</b>
+	 * @author james_2pes9af
+	 */
+	public Node findNode(int ID){
+		Node current = origin;
+		while(current.getNext()!=null){
+    		if (current.getID()==ID)
+    			return current;
+    		else
+    			current=current.getNext();
+		}
+		return null;
+	}
+	
+	/**
+	 * <b><i>printForward</i></b> - Prints the contents of the list, going forward.
+	 * <p>
+	 * Iterates forward through the list and generates a string that is comma delimited containing the content of each node.
+	 * <p>
+	 * @return <b>String</b> - contains each content in a single string separated by commas.
+	 * @author james_2pes9af
+	 */
 	public String printForward(){
 		String list="";
 		Node current=origin;
@@ -66,8 +115,14 @@ public class LinkedList {
 		return list;
 	}
 	
-	//iterates backwards through the list and returns a string that is comma 
-	//delimited containing the content of each node.
+	/**
+	 * <b><i>printReverse</i></b> - Prints the contents of the list, going backwards.
+	 * <p>
+	 * Iterates backwards through the list and generates a string that is comma delimited containing the content of each node.
+	 * <p>
+	 * @return <b>String</b> - contains each content in a single string separated by commas.
+	 * @author james_2pes9af
+	 */
 	public String printReverse(){
 		String list="";
 		Node current=end;
