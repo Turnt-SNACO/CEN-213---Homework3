b10b83bdaed808939edab9da875526a9a9d2ac7a
Turnt-SNACO <janderson5@albany.edu>
2017-03-10 16:09:55 -0500

    Changed Node to take Comparable data type instead of restricting to string so the linkedlist is more versitile.

--- AndersonJamesH03/src/Node.java
+++ AndersonJamesH03/src/Node.java
@@ -1,4 +1,45 @@
 
 public class Node {
-
+	
+	private Node previous,next;
+	private int ID;
+	private Comparable content;
+	
+	//Construct the node with an ID and content
+	public Node(int ID, Comparable content){
+		this.ID=ID;		this.content=content;
+		this.next=null; this.previous=null;
+	}
+	
+	//returns the previous node
+	public Node getPrevious() {
+		return previous;
+	}
+	
+	//set the previous node
+	public void setPrevious(Node previous) {
+		this.previous = previous;
+	}
+	
+	//returns the next node
+	public Node getNext() {
+		return next;
+	}
+	
+	//sets next node
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Comparable getContent() {
+		return content;
+	}
+	public void setContent(Comparable content) {
+		this.content = content;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
 }
