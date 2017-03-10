a1e977089e990312c77193ba7d783ebd9255fbb5
Turnt-SNACO <janderson5@albany.edu>
2017-03-10 17:00:25 -0500

    Updated tests.

--- AndersonJamesH03/src/Main.java
+++ AndersonJamesH03/src/Main.java
@@ -7,12 +7,29 @@ public class Main {
 		Node spade = new Node (2, "spade");
 		Node diamond = new Node (3, "diamond");
 		
+		//test add to end
 		l.addToEnd(ace);
 		l.addToEnd(club);
 		l.addToEnd(spade);
 		l.addToEnd(diamond);
 		
+		//test prints
 		System.out.println(l.printForward());
 		System.out.println(l.printReverse());
+		System.out.println(l.getSize());
+		
+		//test finds
+		System.out.println("Found ace at ID: "+l.findNode("ace").getID());
+		System.out.println("Found ID 0 with content: "+l.findNode(0).getContent());
+		
+		//test deletes
+		l.deleteNode(club);
+		
+		//post delete print check
+		System.out.println(l.printForward());
+		System.out.println(l.printReverse());
+		System.out.println(l.getSize());
+		
+		
 	}
 }
